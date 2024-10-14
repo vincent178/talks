64a6c6068560b2d2782c7389f19c609f8f19d31b
vincent178 <vh7157@gmail.com>
2024-10-14 10:43:45 +0800

    update the build script

--- proto-compatibility/build.sh
+++ proto-compatibility/build.sh
@@ -4,9 +4,9 @@ versions=(v1 v2 v3 v4 v5 v6)
 
 for v in ${versions[@]}; do
     mkdir -p $v
-    # cp proto/hello.proto $v
+    # cp proto/hello.proto $v -- oneoff
     echo "building $v/hello.proto"
-    rm -rf $v/$v
+
     protoc --go_out=$v \
         --go_opt=module=github.com/vincent178/talks/proto-compatibility/$v \
         --go-grpc_out=$v \
@@ -14,11 +14,5 @@ for v in ${versions[@]}; do
         $v/hello.proto
 done
 
+echo "convert slide markdown to pdf"
 pandoc -t beamer slide.txt -o proto-compatibility.pdf
-
-# docker run --rm -it \
-#   -v $(pwd):/workspace \
-#   linuxserver/ffmpeg \
-#   -i /workspace/frames%04d.png \
-#   -vcodec mjepg \
-#   -y /workspace/movie.mp4
